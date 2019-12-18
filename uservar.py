8b6bfcfd46a133b7bc143e67c599ba8c19423656
Reddit Reaper <35949207+reddit-reaper@users.noreply.github.com>
2019-12-18 18:19:20 -0500

    rebase from OpenWizard 1.2.8

--- uservar.py
+++ uservar.py
@@ -1,129 +1,126 @@
-import os, xbmc, xbmcaddon
+import xbmcaddon
+
+import os
 
 #########################################################
-### Global Variables ####################################
+#         Global Variables - DON'T EDIT!!!              #
 #########################################################
-PATH           = xbmcaddon.Addon().getAddonInfo('path')
-ART            = os.path.join(PATH, 'resources', 'art')
+ADDON_ID = xbmcaddon.Addon().getAddonInfo('id')
+PATH = xbmcaddon.Addon().getAddonInfo('path')
+ART = os.path.join(PATH, 'resources', 'media')
 #########################################################
 
 #########################################################
-### User Edit Variables #################################
+#        User Edit Variables                            #
 #########################################################
-ADDON_ID       = xbmcaddon.Addon().getAddonInfo('id')
-ADDONTITLE     = 'Reaper Wizard'
-BUILDERNAME    = 'Reddit Reaper'
-EXCLUDES       = [ADDON_ID, 'repository.redditreaper', 'plugin.program.reaperwizard']
-# Enable/Disable the text file caching with 'Yes' or 'No' and age being how often it rechecks in minutes
-CACHETEXT      = 'Yes'
-CACHEAGE       = 30
+ADDONTITLE = 'Reaper Wizard'
+BUILDERNAME = 'Reddit Reaper'
+EXCLUDES = [ADDON_ID, 'repository.redditreaper']
 # Text File with build info in it.
-BUILDFILE      = 'https://raw.githubusercontent.com/reddit-reaper/plugin.program.reaperwizard/master/resources/text/builds.txt'
+BUILDFILE = 'https://raw.githubusercontent.com/reddit-reaper/plugin.program.reaperwizard/master/resources/text/builds.txt'
 # How often you would like it to check for build updates in days
 # 0 being every startup of kodi
-UPDATECHECK    = 0
+UPDATECHECK = 0
 # Text File with apk info in it.  Leave as 'http://' to ignore
-APKFILE        = 'http://'
+APKFILE = 'http://'
 # Text File with Youtube Videos urls.  Leave as 'http://' to ignore
-YOUTUBETITLE   = ''
-YOUTUBEFILE    = 'http://'
+YOUTUBETITLE = ''
+YOUTUBEFILE = 'http://'
 # Text File for addon installer.  Leave as 'http://' to ignore
-ADDONFILE      = 'http://'
+ADDONFILE = 'http://'
 # Text File for advanced settings.  Leave as 'http://' to ignore
-ADVANCEDFILE   = 'https://raw.githubusercontent.com/reddit-reaper/plugin.program.reaperwizard/master/resources/text/advanced.txt'
+ADVANCEDFILE = 'https://raw.githubusercontent.com/reddit-reaper/plugin.program.reaperwizard/master/resources/text/advanced.txt'
 #########################################################
 
 #########################################################
-### Theming Menu Items ##################################
+#        Theming Menu Items                             #
 #########################################################
 # If you want to use locally stored icons the place them in the Resources/Art/
 # folder of the wizard then use os.path.join(ART, 'imagename.png')
 # do not place quotes around os.path.join
 # Example:  ICONMAINT     = os.path.join(ART, 'mainticon.png')
-#           ICONSETTINGS  = 'http://aftermathwizard.net/repo/wizard/settings.png'
+#           ICONSETTINGS  = 'https://www.yourhost.com/repo/wizard/settings.png'
 # Leave as http:// for default icon
-ICONBUILDS     = 'http://'
-ICONMAINT      = 'http://'
-ICONSPEED      = 'http://'
-ICONAPK        = 'http://'
-ICONADDONS     = 'http://'
-ICONYOUTUBE    = 'http://'
-ICONSAVE       = 'http://'
-ICONTRAKT      = 'http://'
-ICONREAL       = 'http://'
-ICONLOGIN      = 'http://'
-ICONCONTACT    = 'http://'
-ICONSETTINGS   = 'http://'
-# Hide the section seperators 'Yes' or 'No'
-HIDESPACERS    = 'No'
-# Character used in seperator
-SPACER         = '='
+ICONBUILDS = 'http://'
+ICONMAINT = 'http://'
+ICONSPEED = 'http://'
+ICONAPK = 'http://'
+ICONADDONS = 'http://'
+ICONYOUTUBE = 'http://'
+ICONSAVE = 'http://'
+ICONTRAKT = 'http://'
+ICONREAL = 'http://'
+ICONLOGIN = 'http://'
+ICONCONTACT = 'http://'
+ICONSETTINGS = 'http://'
+# Hide the section separators 'Yes' or 'No'
+HIDESPACERS = 'No'
+# Character used in separator
+SPACER = '='
 
 # You can edit these however you want, just make sure that you have a %s in each of the
 # THEME's so it grabs the text from the menu item
-COLOR1         = 'dodgerblue'
-COLOR2         = 'white'
-# Primary menu items   / %s is the menu item and is required
-THEME1         = '[COLOR '+COLOR1+'][B][I]([COLOR '+COLOR2+']Aftermath[/COLOR])[/I][/B][/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
-# Build Names          / %s is the menu item and is required
-THEME2         = '[COLOR '+COLOR2+']%s[/COLOR]'
-# Alternate items      / %s is the menu item and is required
-THEME3         = '[COLOR '+COLOR1+']%s[/COLOR]'
-# Current Build Header / %s is the menu item and is required
-THEME4         = '[COLOR '+COLOR1+']Current Build:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
-# Current Theme Header / %s is the menu item and is required
-THEME5         = '[COLOR '+COLOR1+']Current Theme:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
+COLOR1 = 'dodgerblue'
+COLOR2 = 'white'
+# Primary menu items   / {0} is the menu item and is required
+THEME1 = '[COLOR '+COLOR1+'][I]([COLOR '+COLOR1+'][B]Open[/B][/COLOR][COLOR ' + COLOR2 + ']Wizard[COLOR ' + COLOR1 + '])[/I][/COLOR] [COLOR '+COLOR2+']{0}[/COLOR]'
+# Build Names          / {0} is the menu item and is required
+THEME2 = '[COLOR '+COLOR2+']{0}[/COLOR]'
+# Alternate items      / {0} is the menu item and is required
+THEME3 = '[COLOR '+COLOR1+']{0}[/COLOR]'
+# Current Build Header / {0} is the menu item and is required
+THEME4 = '[COLOR '+COLOR1+']Current Build:[/COLOR] [COLOR '+COLOR2+']{0}[/COLOR]'
+# Current Theme Header / {0} is the menu item and is required
+THEME5 = '[COLOR '+COLOR1+']Current Theme:[/COLOR] [COLOR '+COLOR2+']{0}[/COLOR]'
 
 # Message for Contact Page
 # Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
-HIDECONTACT    = 'No'
+HIDECONTACT = 'No'
 # You can add \n to do line breaks
-CONTACT        = 'Thank you for choosing Reaper Wizard.\n\nContact me on Github at http://www.github.com/reddit-reaper/plugin.program.reaperwizard/'
-#Images used for the contact window.  http:// for default icon and fanart
-CONTACTICON    = 'http://'
-CONTACTFANART  = 'http://'
+CONTACT = 'Thank you for choosing Reaper Wizard.\n\nContact me on Github at http://www.github.com/reddit-reaper/plugin.program.reaperwizard/'
+# Images used for the contact window.  http:// for default icon and fanart
+CONTACTICON = 'http://'
+CONTACTFANART = 'http://'
 #########################################################
 
 #########################################################
-### Auto Update                   #######################
-###        For Those With No Repo #######################
+#        Auto Update For Those With No Repo             #
 #########################################################
 # Enable Auto Update 'Yes' or 'No'
-AUTOUPDATE     = 'No'
+AUTOUPDATE = 'No'
 # Url to wizard version
-WIZARDFILE     = BUILDFILE
+WIZARDFILE = BUILDFILE
 #########################################################
 
 #########################################################
-### Auto Install                 ########################
-###        Repo If Not Installed ########################
+#        Auto Install Repo If Not Installed             #
 #########################################################
 # Enable Auto Install 'Yes' or 'No'
-AUTOINSTALL    = 'Yes'
+AUTOINSTALL = 'Yes'
 # Addon ID for the repository
-REPOID         = 'repository.redditreaeper'
+REPOID = 'repository.redditreaeper'
 # Url to Addons.xml file in your repo folder(this is so we can get the latest version)
-REPOADDONXML   = 'https://raw.githubusercontent.com/reddit-reaper/Addons/master/addons.xml'
+REPOADDONXML = 'https://raw.githubusercontent.com/reddit-reaper/Addons/master/repository.redditreaper/addon.xml'
 # Url to folder zip is located in
-REPOZIPURL     = 'https://github.com/reddit-reaper/Addons/raw/master/repository.redditreaper/'
+REPOZIPURL = 'https://github.com/reddit-reaper/Addons/raw/master/repository.redditreaper/'
 #########################################################
 
 #########################################################
-### Notification Window #################################
+#        Notification Window                            #
 #########################################################
 # Enable Notification screen Yes or No
-ENABLE         = 'Yes'
+ENABLE = 'Yes'
 # Url to notification file
-NOTIFICATION   = 'https://raw.githubusercontent.com/reddit-reaper/plugin.program.reaperwizard/master/resources/text/notify.txt'
+NOTIFICATION = 'https://raw.githubusercontent.com/reddit-reaper/plugin.program.reaperwizard/master/resources/text/notify.txt'
 # Use either 'Text' or 'Image'
-HEADERTYPE     = 'Text'
+HEADERTYPE = 'Text'
 # Font size of header
-FONTHEADER     = 'Font14'
-HEADERMESSAGE  = 'Reaper Wizard'
+FONTHEADER = 'Font14'
+HEADERMESSAGE = 'Reaper Wizard'
 # url to image if using Image 424x180
-HEADERIMAGE    = 'http://'
+HEADERIMAGE = 'http://'
 # Font for Notification Window
-FONTSETTINGS   = 'Font13'
+FONTSETTINGS = 'Font13'
 # Background for Notification Window
-BACKGROUND     = 'http://'
+BACKGROUND = 'http://'
 #########################################################
